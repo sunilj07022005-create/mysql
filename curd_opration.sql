1371c6cf4e347638682f8fd78ea7a9fdf09e9bee
sunilj07022005 <sunilj07022005@gmail.com>
2026-08-15 14:10:18 +0530

    frist commit

---
+++ curd_opration.sql
@@ -0,0 +1,9 @@
+create database basic_opration;
+use basic_opration;
+create table basic_opration(id int, name varchar(20),salary double(6,2),address varchar(30),email varchar(25),from_time timestamp default current_timestamp(),mobile varchar(10));
+
+alter table basic_opration modify id int auto_increment primary key ;
+Alter table basic_opration modify salary double(10,2);
+insert into basic_opration (name, salary,address,email,mobile)values('karan',24500.50,'hingne_colani','karan12@gmail.com',"8786965412");
+select * from basic_opration;
+
