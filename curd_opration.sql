a04b83d80a1bc34ccb656a72f5afc3a4edcae4a0
sunilj07022005 <sunilj07022005@gmail.com>
2026-08-15 14:41:12 +0530

    seconde commit

--- curd_opration.sql
+++ curd_opration.sql
@@ -6,4 +6,8 @@ alter table basic_opration modify id int auto_increment primary key ;
 Alter table basic_opration modify salary double(10,2);
 insert into basic_opration (name, salary,address,email,mobile)values('karan',24500.50,'hingne_colani','karan12@gmail.com',"8786965412");
 select * from basic_opration;
+insert into basic_opration (name, salary,address,email,mobile)values('ram',79500.50,'digamber_colani','ram12@gmail.com',"7030660282"),
+('arjun',76000.67,'aumdumber colni','arjun12@gmial.com',"988089009"),
+('yash',50700.69,'warjw-marwadi','yash12@gmail.com','8909679856');
+
 
