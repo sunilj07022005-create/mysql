c86467d6f14161940d650ac8438cb83e2c2f5fbe
sunilj07022005 <sunilj07022005@gmail.com>
2026-08-25 00:05:18 +0530

    second commit

--- Mysql7_TCL.sql
+++ Mysql7_TCL.sql
@@ -1,5 +1,4 @@
 use db1;
-select * from bank;
 
 SELECT @@autocommit=1;   -- used to check the auto commit on or not 
 SELECT @@in_transaction;
@@ -10,11 +9,24 @@ insert into bank values (104,'arjun',40000.70);
 -- 103	Amit	20000.70
 -- 104	arjun	30000.70
 
-start transaction;		-- is on the temporary mode   evevery time you want run program newly form the start transaction 
+start transaction;		-- is on the temporary mode   ->every time you want run program newly form the start transaction 
 update bank set balance = 30002.40 where account_id=104;
 rollback;
 
-select * from bank where account_id=104;
+insert into bank values (105,'dnayu',50000.80);   
+select * from bank where account_id=105;
 
-SELECT VERSION();
-SHOW CREATE TABLE bank;
+commit;
+rollback;   -- afetr commit you can't rollback 
+
+-- save point use when we want some are commit or some are rollback
+start transaction;
+update bank set balance=50000.70 where account_id=105;
+savepoint sp1;					-- create savepoint
+update bank set balance=30000.70 where account_id=104;
+rollback to savepoint sp1;		-- rollback up to savepoint
+update bank set name ="karn" where balance =30001.40;
+release savepoint sp1;  -- release savepoint mean deleting savepoint because you no longer need it
+commit;
+
+select * from bank;
